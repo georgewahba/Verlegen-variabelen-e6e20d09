b27f7ac837e64b02e112af11dd333ddc78f32fae
GeorgeWahba <george-wahba@hotmail.com>
2020-02-06 10:28:56 +0100

    Uitwerking van de exercise

---
+++ script.js
@@ -0,0 +1,7 @@
+const number = 3.14;
+
+function calculate() {
+    let nummer = document.getElementById('number').value;
+    document.getElementById('antwoord').innerHTML = document.getElementById('number').value * number;
+    document.getElementById('oppervlakte').innerHTML = nummer * nummer * number * 0.25;
+}
